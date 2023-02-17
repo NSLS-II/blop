2870bd31fca349d0548e58445f89b094f3f16bc9
Thomas Morris <tmorris@bnl.gov>
2023-02-17 11:00:31 -0500

    further GP development

--- bloptools/gp/utils.py
+++ bloptools/gp/utils.py
@@ -2,89 +2,81 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 2a8fbcf15463292b877801d3a77e6e8a22c944d3
-def parse_images(images, index_to_parse, n_max_median=1024, verbose=False):
-    '''
-    Parse a stack of images of shape (n_f, n_y, n_x)
-    '''
-    
-    # sample at most n_max_median points in estimating the background
-    background = np.median(images[np.unique(np.linspace(0,len(images)-1,n_max_median).astype(int))], axis=0)
-    
-    beam_stats = pd.DataFrame(columns=['x_min', 'x_max', 'y_min', 'y_max', 'flux', 'separability'])
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
 
-    for i, image in enumerate(images):
+def get_routing(origin, points):
+    '''
+    Finds an efficient routing between $n$ points, after normalizing each dimension.
+    Returns $n-1$ indices, ignoring the zeroeth index (the origin).
+    '''
+    
+    _points = np.r_[np.atleast_2d(origin), points]
+    
+    rel_points = _points / _points.std(axis=0)
+
+    #delay_matrix = gpo.delay_estimate(rel_points[:,None,:] - rel_points[None,:,:])
+    delay_matrix  = np.sqrt(np.square(rel_points[:,None,:] - rel_points[None,:,:]).sum(axis=-1))
+    delay_matrix *= 1000
+    
+    manager = pywrapcp.RoutingIndexManager(len(_points), 1, 0) # number of depots, number of salesmen, starting index
+    routing = pywrapcp.RoutingModel(manager)
+
+    def delay_callback(from_index, to_index):
+        to_node = manager.IndexToNode(to_index)
+        if to_node == 0: return 0 # it is free to return to the depot from anywhere; we just won't do it
+        from_node = manager.IndexToNode(from_index)
+        return delay_matrix[from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(delay_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    solution = routing.SolveWithParameters(search_parameters)
+    dir(solution)
+
+    index = routing.Start(0)
+    route_indices, route_delays = [], []
+    while not routing.IsEnd(index):
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_delays.append(routing.GetArcCostForVehicle(previous_index, index, 0))
+        route_indices.append(index)
         
-        if not i in index_to_parse:
-            continue
-        
-        beam_stats.loc[i] = _get_beam_stats(image)# - background)
-        if verbose: print(i); ip.display.clear_output(wait=True)
-        
-    return beam_stats
+    return np.array(route_indices)[:-1] - 1, route_delays[:-1]
 
-def _get_beam_stats(image, beam_prop=0.8):
+
+def get_movement_time(x,v_max,a):
     '''
-    Parse the beam from an image. Returns the bounding box, along with a flux 
-    estimate and a goodness of fit parameter. This should go off without a hitch
-    as long as beam_prop is less than 1. 
+    How long does it take an object to go distance $x$ with acceleration $a$ and maximum velocity $v_max$?
+    That's what this function answers. 
     '''
-    
-    n_y, n_x = image.shape
-    u, s, v = np.linalg.svd(image - image.mean())
-    
-    separability = np.square(s[0])/np.square(s).sum()
-    
-    # q refers to "quantile"
-    q_min, q_max = 0.5 * (1 - beam_prop), 0.5 * (1 + beam_prop)
-    
-    # these represent the cumulative proportion of the beam, as captured by the SVD.
-    cs_beam_x = np.cumsum(v[0]) / np.sum(v[0])
-    cs_beam_y = np.cumsum(u[:,0]) / np.sum(u[:,0])
-<<<<<<< HEAD
+    return 2*np.sqrt(np.abs(x)/a)*(np.abs(x)<v_max**2/a)+(np.abs(x)/v_max+v_max/a)*(np.abs(x)>v_max**2/a) # it works lol
 
-    # the first coordinate where the cumulative beam is greater than the minimum
-    i_q_min_x = np.where((cs_beam_x[1:] > q_min) & (cs_beam_x[:-1] < q_min))[0][0]
-    i_q_min_y = np.where((cs_beam_y[1:] > q_min) & (cs_beam_y[:-1] < q_min))[0][0]
-    
-    # the last coordinate where the cumulative beam is less than the maximum
-    i_q_max_x = np.where((cs_beam_x[1:] > q_max) & (cs_beam_x[:-1] < q_max))[0][-1]
-    i_q_max_y = np.where((cs_beam_y[1:] > q_max) & (cs_beam_y[:-1] < q_max))[0][-1]
-
-    # interpolate, so that we can go finer than one pixel. this quartet is the "bounding box"
-    x_min = np.interp(q_min, cs_beam_x[[i_q_min_x,i_q_min_x+1]], [i_q_min_x,i_q_min_x+1])
-    x_max = np.interp(q_max, cs_beam_x[[i_q_max_x,i_q_max_x+1]], [i_q_max_x,i_q_max_x+1])
-    y_min = np.interp(q_min, cs_beam_y[[i_q_min_y,i_q_min_y+1]], [i_q_min_y,i_q_min_y+1])
-    y_max = np.interp(q_max, cs_beam_y[[i_q_max_y,i_q_max_y+1]], [i_q_max_y,i_q_max_y+1])
-    
-    # the center and width of the bounding box
-    c_x, w_x = 0.5 * (x_min + x_max), x_max - x_min
-    c_y, w_y = 0.5 * (y_min + y_max), y_max - y_min
-    
-    flux = image[int(np.max([0,c_y-w_y])):int(np.min([c_y+w_y,n_y-1]))][:,int(np.max([0,c_x-w_x])):int(np.min([c_x+w_x,n_x-1]))].sum()
-    
-    return x_min, x_max, y_min, y_max, flux, separability
-=======
-=======
 def parse_images(images, extents=None, index_to_parse=None, n_max_median=1024, remove_background=False, verbose=False):
     '''
     Parse a stack of images of shape (n_f, n_y, n_x)
     '''
-    if extents is None: extents = [None for i in range(len(images))]
-    if index_to_parse is None: index_to_parse = np.arange(len(images))
+    n_f, n_y, n_x = images.shape
+
+    if extents is None: extents = [None for i in range(n_f)]
+    if index_to_parse is None: index_to_parse = np.arange(n_f)
 
     # sample at most n_max_median points in estimating the background
     background = np.median(images[np.unique(np.linspace(0,len(images)-1,n_max_median).astype(int))], axis=0) if remove_background else 0
-    beam_stats = pd.DataFrame(columns=['x_min', 'x_max', 'y_min', 'y_max', 'flux', 'maximum', 'separability'])
+    beam_stats = pd.DataFrame(columns=['x_min', 'x_max', 'y_min', 'y_max', 'rel_x_min', 'rel_x_max', 'rel_y_min', 'rel_y_max', 'flux', 'maximum', 'separability'], dtype=float)
 
     for i, (image, extent) in enumerate(zip(images, extents)):
+
         if not i in index_to_parse:
             continue
+        if extent is None: 
+            extent = np.array([0, n_y, 0, n_x])
 
-        beam_stats.loc[i] = _get_beam_stats(image - background, extent)
+        beam_stats.loc[i, ['rel_x_min', 'rel_x_max', 'rel_y_min', 'rel_y_max', 'flux', 'maximum', 'separability']] = _get_beam_stats(image - background, beam_prop=0.8)
+        beam_stats.loc[i, ['x_min', 'x_max']] = np.interp(beam_stats.loc[i, ['rel_x_min', 'rel_x_max']].values, [0, 1], extent[2:])
+        beam_stats.loc[i, ['y_min', 'y_max']] = np.interp(beam_stats.loc[i, ['rel_y_min', 'rel_y_max']].values, [0, 1], extent[:-2])
         #if verbose: print(i); ip.display.clear_output(wait=True)
 
     beam_stats['w_x'] = beam_stats.x_max - beam_stats.x_min
@@ -92,28 +84,25 @@ def parse_images(images, extents=None, index_to_parse=None, n_max_median=1024, r
 
     beam_stats['pixel_area'] = (images > 0.05 * images.max(axis=(1,2))[:,None,None]).sum(axis=(1,2))
 
-    beam_stats['fitness'] = beam_stats.separability / (beam_stats.w_x**2 + beam_stats.w_y**2)
+    beam_stats['fitness'] = beam_stats.flux * beam_stats.separability / (beam_stats.w_x**2 + beam_stats.w_y**2)
 
-    beam_stats['fitness'] = beam_stats['flux'] / beam_stats['pixel_area']
+    #beam_stats['fitness'] = beam_stats['flux'] / beam_stats['pixel_area']
 
-    OOB_buffer = 16
+    OOB_rel_buffer = 1/32 # out-of-bounds relative buffer
 
-    n_f, n_y, n_x = images.shape
+    bad  = beam_stats.rel_x_min.values < OOB_rel_buffer
+    bad |= beam_stats.rel_x_max.values > 1 - OOB_rel_buffer
+    bad |= beam_stats.rel_y_min.values < OOB_rel_buffer
+    bad |= beam_stats.rel_y_max.values > 1 - OOB_rel_buffer
+    bad |= beam_stats.separability.values < 0.5 # at least half the variance must be explained by a beam
 
-    bad  = beam_stats.x_min.values < OOB_buffer
-    bad |= beam_stats.x_max.values > n_x - 1 - OOB_buffer
-    bad |= beam_stats.y_min.values < OOB_buffer
-    bad |= beam_stats.y_max.values > n_y - 1 - OOB_buffer
-    bad |= beam_stats.separability.values < 0.8
-    #bad |= images.sum(axis=(1,2)) == 0
-
-    beam_stats.loc[bad, 'fitness'] = np.nan
+    beam_stats.loc[bad, 'fitness'] = np.nan # set the fitness of questionable beams to nan
 
     return beam_stats
 
-def _get_beam_stats(image, extent, beam_prop=0.5):
+def _get_beam_stats(image, beam_prop=0.5):
     '''
-    Parse the beam from an image. Returns the bounding box, along with a flux
+    Parse the beam from an image. Returns the normalized bounding box, along with a flux
     estimate and a goodness of fit parameter. This should go off without a hitch
     as long as beam_prop is less than 1.
     '''
@@ -123,12 +112,11 @@ def _get_beam_stats(image, extent, beam_prop=0.5):
     if image.sum() == 0:
         return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
 
-    if extent is None: extent = np.array([0, n_y, 0, n_x])
+    
 
     # print(f'{extent = }')
 
     u, s, v = np.linalg.svd(image - image.mean())
-
     separability = np.square(s[0])/np.square(s).sum()
 
     # q refers to "quantile"
@@ -138,105 +126,21 @@ def _get_beam_stats(image, extent, beam_prop=0.5):
     cs_beam_x = np.cumsum(v[0]) / np.sum(v[0])
     cs_beam_y = np.cumsum(u[:,0]) / np.sum(u[:,0])
     cs_beam_x[0], cs_beam_y[0] = 0, 0 # change this lol
->>>>>>> abd3ed6a693f3b1be896bd88f9ba9aa1a1147487
 
     # the first coordinate where the cumulative beam is greater than the minimum
     i_q_min_x = np.where((cs_beam_x[1:] > q_min) & (cs_beam_x[:-1] < q_min))[0][0]
     i_q_min_y = np.where((cs_beam_y[1:] > q_min) & (cs_beam_y[:-1] < q_min))[0][0]
-<<<<<<< HEAD
     
-=======
-
->>>>>>> abd3ed6a693f3b1be896bd88f9ba9aa1a1147487
     # the last coordinate where the cumulative beam is less than the maximum
     i_q_max_x = np.where((cs_beam_x[1:] > q_max) & (cs_beam_x[:-1] < q_max))[0][-1]
     i_q_max_y = np.where((cs_beam_y[1:] > q_max) & (cs_beam_y[:-1] < q_max))[0][-1]
 
-    # interpolate, so that we can go finer than one pixel. this quartet is the "bounding box"
-<<<<<<< HEAD
-    x_min = np.interp(q_min, cs_beam_x[[i_q_min_x,i_q_min_x+1]], [i_q_min_x,i_q_min_x+1])
-    x_max = np.interp(q_max, cs_beam_x[[i_q_max_x,i_q_max_x+1]], [i_q_max_x,i_q_max_x+1])
-    y_min = np.interp(q_min, cs_beam_y[[i_q_min_y,i_q_min_y+1]], [i_q_min_y,i_q_min_y+1])
-    y_max = np.interp(q_max, cs_beam_y[[i_q_max_y,i_q_max_y+1]], [i_q_max_y,i_q_max_y+1])
-    
-    # the center and width of the bounding box
-    c_x, w_x = 0.5 * (x_min + x_max), x_max - x_min
-    c_y, w_y = 0.5 * (y_min + y_max), y_max - y_min
-    
-    flux = image[int(np.max([0,c_y-w_y])):int(np.min([c_y+w_y,n_y-1]))][:,int(np.max([0,c_x-w_x])):int(np.min([c_x+w_x,n_x-1]))].sum()
-    
-    return x_min, x_max, y_min, y_max, flux, separability
-=======
+    # interpolate, so that we can go finer than one pixel. this quartet is the "bounding box", and each value is between 0 and 1.
     # (let's make this more efficient later)
-    i_x_min = np.interp(q_min, cs_beam_x[[i_q_min_x,i_q_min_x+1]], [i_q_min_x,i_q_min_x+1])
-    i_x_max = np.interp(q_max, cs_beam_x[[i_q_max_x,i_q_max_x+1]], [i_q_max_x,i_q_max_x+1])
-    i_y_min = np.interp(q_min, cs_beam_y[[i_q_min_y,i_q_min_y+1]], [i_q_min_y,i_q_min_y+1])
-    i_y_max = np.interp(q_max, cs_beam_y[[i_q_max_y,i_q_max_y+1]], [i_q_max_y,i_q_max_y+1])
+    x_min = np.interp(q_min, cs_beam_x[[i_q_min_x,i_q_min_x+1]], [i_q_min_x,i_q_min_x+1]) / n_x
+    x_max = np.interp(q_max, cs_beam_x[[i_q_max_x,i_q_max_x+1]], [i_q_max_x,i_q_max_x+1]) / n_x
+    y_min = np.interp(q_min, cs_beam_y[[i_q_min_y,i_q_min_y+1]], [i_q_min_y,i_q_min_y+1]) / n_y
+    y_max = np.interp(q_max, cs_beam_y[[i_q_max_y,i_q_max_y+1]], [i_q_max_y,i_q_max_y+1]) / n_y
 
-    x_min = np.interp(i_x_min, [0, n_x], extent[2:])
-    x_max = np.interp(i_x_max, [0, n_x], extent[2:])
-    y_min = np.interp(i_y_min, [0, n_y], extent[:2])
-    y_max = np.interp(i_y_max, [0, n_y], extent[:2])
+    return x_min, x_max, y_min, y_max, s[0] * np.outer(u[:,0], v[0]).sum(), image.max() - image.mean(), separability
 
-    # the center and width of the bounding box
-    c_x, w_x = 0.5 * (x_min + x_max), x_max - x_min
-    c_y, w_y = 0.5 * (y_min + y_max), y_max - y_min
-
-    i_c_x, i_w_x = 0.5 * (i_x_min + i_x_max), i_x_max - i_x_min
-    i_c_y, i_w_y = 0.5 * (i_y_min + i_y_max), i_y_max - i_y_min
-
-    flux = image[int(np.max([0,i_c_y-i_w_y])):int(np.min([i_c_y+i_w_y,n_y-1]))][:,int(np.max([0,i_c_x-i_w_x])):int(np.min([i_c_x+i_w_x,n_x-1]))].sum()
-
-    return x_min, x_max, y_min, y_max, flux, image.max() - image.mean(), separability
->>>>>>> abd3ed6a693f3b1be896bd88f9ba9aa1a1147487
->>>>>>> 2a8fbcf15463292b877801d3a77e6e8a22c944d3
-
-def process_beam(image, separable_threshold=0.1):
-
-    gaussian = lambda t, a, c, s, o, const : a * np.exp(-np.abs((t-c)/s)**o) + const
-
-    def get_profile_pars(profile):
-
-        ydata = profile.copy()
-        ndata = len(ydata)
-        xdata = np.arange(ndata).astype(float)
-
-        p0 = [ydata.ptp(), np.argmax(ydata), 1e-1 * ndata, 2, ydata.min()]
-
-        bounds = [[0,0,0,0,-np.inf],[np.inf,ndata-1,ndata,16,np.inf]]
-
-        fit_pars, fit_cpars = sp.optimize.curve_fit(gaussian, xdata, ydata, p0=p0, bounds=bounds)
-
-        RSS = np.square(ydata - gaussian(xdata, *fit_pars)).sum()
-        TSS = np.square(ydata).sum()
-
-        co_r_2 = RSS / TSS
-
-        return fit_pars, np.sqrt(np.diag(fit_cpars)), co_r_2
-
-    ny, nx = image.shape
-
-    u,s,v = np.linalg.svd(image - np.median(image))
-    (_, cx, sx, ox, _), (_, cx_err, sx_err, _, _), x_cr2 = get_profile_pars(v[0])
-    (_, cy, sy, oy, _), (_, cy_err, sy_err, _, _), y_cr2 = get_profile_pars(u[:,0])
-
-    print(f'cx = {cx:.03f} ± {cx_err:.03f} | sx = {sx:.03f} ± {sx_err:.03f} | {x_cr2:.03f}')
-    print(f'cy = {cy:.03f} ± {cy_err:.03f} | sy = {sy:.03f} ± {sy_err:.03f} | {y_cr2:.03f}')
-
-    if (x_cr2 > separable_threshold) or (y_cr2 > separable_threshold): return np.nan * image
-
-    crop_sigma = 3
-
-    x_crop_min = np.maximum(int(cx - crop_sigma * sx), 0)
-    x_crop_max = np.minimum(int(cx + crop_sigma * sx), nx - 1)
-    y_crop_min = np.maximum(int(cy - crop_sigma * sy), 0)
-    y_crop_max = np.minimum(int(cy + crop_sigma * sy), ny - 1)
-
-    cropped_beam = image[y_crop_min:y_crop_max, x_crop_min:x_crop_max]
-
-    cropped_ny, cropped_nx = cropped_beam.shape
-    on_edge = np.ones((cropped_ny, cropped_nx)).astype(bool)
-    on_edge[1:-1, 1:-1] = False
-    background_median = np.median(cropped_beam[on_edge])
-
-    return cropped_beam - background_median
