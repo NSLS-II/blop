ab66f39394841b749b8949a4049cd8b0f09d219f
Max Rakitin <mrakitin@bnl.gov>
2023-04-01 18:15:22 -0400

    Rework the code to exclude RE from inside the Optimizer methods

--- bloptools/gp/plans.py
+++ bloptools/gp/plans.py
@@ -4,28 +4,30 @@ import bluesky.plan_stubs as bps
 import bluesky.plans as bp  # noqa F401
 
 
-def take_background(self):
+def take_background(shutter, detectors):
     timeout = 10
 
-    yield from bps.mv(self.shutter.close_cmd, 1)
+    yield from bps.mv(shutter.close_cmd, 1)
     yield from bps.sleep(2.0)
 
     start_time = ttime.monotonic()
-    while (ttime.monotonic() - start_time < timeout) and (self.shutter.status.get() != 1):
-        print(f"Shutter not closed, retrying ... (closed_status = {self.shutter.status.get()})")
+    while (ttime.monotonic() - start_time < timeout) and (shutter.status.get(as_string=True) == "Open"):
+        print(f"Shutter not closed, retrying ... (closed_status = {shutter.status.get(as_string=True)})")
         yield from bps.sleep(1.0)
-        yield from bps.mv(self.shutter.close_cmd, 1)
+        yield from bps.mv(shutter.close_cmd, 1)
 
-    yield from bp.count(self.detectors)
+    uid = yield from bp.count(detectors)
 
-    yield from bps.mv(self.shutter.open_cmd, 1)
+    yield from bps.mv(shutter.open_cmd, 1)
     yield from bps.sleep(2.0)
 
     timeout = 10
     start_time = ttime.monotonic()
-    while (ttime.monotonic() - start_time < timeout) and (self.shutter.status.get() != 0):
-        print(f"Shutter not open, retrying ... (closed_status = {self.shutter.status.get()})")
+    while (ttime.monotonic() - start_time < timeout) and (shutter.status.get(as_string=True) != "Open"):
+        print(f"Shutter not open, retrying ... (closed_status = {shutter.status.get(as_string=True)})")
         yield from bps.sleep(1.0)
-        yield from bps.mv(self.shutter.open_cmd, 1)
+        yield from bps.mv(shutter.open_cmd, 1)
 
     yield from bps.sleep(10.0)
+
+    return uid
