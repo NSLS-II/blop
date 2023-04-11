1db4c03fd6060837ac61207538c1fb25ca4c754b
Thomas Morris <tmorris@bnl.gov>
2023-04-11 11:31:08 -0400

    fixed benchmark/py

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -11,29 +11,39 @@ import numpy as np
 
 from bloptools import gp
 
-bo = gp.BayesianOptimizer(init_scheme='quasi-random', 
-                            n_init=64, 
-                            detectors=[vstream, I0], 
-                            shutter=psh, 
-                            run_engine=RE, 
-                            db=db, 
-                            dofs=dofs, 
-                            dof_bounds=hard_bounds, 
-                            verbose=True)
+bo = gp.BayesianOptimizer(
+    init_scheme="quasi-random",
+    n_init=64,
+    detectors=[vstream, I0],
+    shutter=psh,
+    run_engine=RE,
+    db=db,
+    dofs=dofs,
+    dof_bounds=hard_bounds,
+    verbose=True,
+)
 
 timeout = 300
 start_time = ttime.monotonic()
 while ttime.monotonic() - start_time < timeout:
     bo.learn(
-        n_iter=1, n_per_iter=16, strategy="ei", greedy=True,
+        n_iter=1,
+        n_per_iter=16,
+        strategy="ei",
+        greedy=True,
     )
 
 timestamps = bo.data.time.astype(int).values / 1e9
 
-plt.plot(timestamps - timestamps[0], [
-            np.nanmax(bo.data.fitness.values[: i + 1]) if not all(np.isnan(bo.data.fitness.values[: i + 1])) else np.nan
-            for i in range(len(bo.data.fitness.values))
-        ])
+plt.plot(
+    timestamps - timestamps[0],
+    [
+        np.nanmax(bo.data.fitness.values[: i + 1])
+        if not all(np.isnan(bo.data.fitness.values[: i + 1]))
+        else np.nan
+        for i in range(len(bo.data.fitness.values))
+    ],
+)
 
 bo.data.drop(columns=[f"vstream_image"], inplace=True)
 bo.data.to_hdf(f"/nsls2/data/tes/shared/config/gpo-benchmarks-230331/{int(timestamps[0])}.h5", "data")
