fe89a6b446e9672342a7d93c0262ccfabcb53a08
Thomas Morris <tmorris@bnl.gov>
2023-04-17 15:10:26 -0400

    added Rosenbrock and StyblinskiTang

--- bloptools/experiments/tests/__init__.py
+++ bloptools/experiments/tests/__init__.py
@@ -7,53 +7,88 @@ def get_dofs(n=2):
     return [Signal(name=f"x{i+1}", value=0) for i in range(n)]
 
 
-class Ackley:
+class BaseOptimizationTest:
+    MIN_SNR = 1e2
+
+    n_dof = 2
+    dofs = get_dofs(n=2)
+    bounds = np.array([[-5.0, +5.0], [-5.0, +5.0]])
+
+    DEPENDENT_COMPONENTS = [f"x{i+1}" for i in range(2)]
+
+    def initialize(self):
+        yield from bps.null()  # do nothing
+
+    def parse_entry(self, entry):
+        X = np.array([getattr(entry, cpt) for cpt in self.DEPENDENT_COMPONENTS])
+        fitness = self.fitness_func(X)
+        return ("fitness"), (fitness)
+
+
+class Ackley(BaseOptimizationTest):
     """
     Ackley's function in $n$ dimensions (https://en.wikipedia.org/wiki/Ackley_function)
     """
 
-    MIN_SNR = 1e1
-
     def __init__(self, n_dof=2):
         self.n_dof = n_dof
-        self.dofs = [getattr(self.dofs_256, f"x{i+1}") for i in range(self.n_dof)]
-        self.bounds = np.array([[0.0, 1.0] for i in range(self.n_dof)])
-
+        self.dofs = get_dofs(n=n_dof)
+        self.bounds = np.array([[-5.0, +5.0] for i in range(self.n_dof)])
         self.DEPENDENT_COMPONENTS = [f"x{i+1}" for i in range(self.n_dof)]
 
-    def initialize(self):
-        yield from bps.null()  # do nothing
-
-    def parse_entry(self, entry):
-        # get the ingredient from our dependent variables
-        x = 4 * (np.c_[[getattr(entry, f"dofs_x{i+1}") for i in range(self.n_dof)]] - 0.5)
-
-        fitness = np.exp(-0.2 * np.sqrt(0.5 * np.sum(x**2))) + 5e-2 * np.exp(0.5 * np.sum(np.cos(2 * np.pi * x)))
-
-        return ("fitness"), (fitness)
+    @staticmethod
+    def fitness_func(X):
+        return (
+            20 * np.exp(-0.2 * np.sqrt(0.5 * np.sum(X**2)))
+            + 5e-2 * np.exp(0.5 * np.sum(np.cos(2 * np.pi * X)))
+            - 20
+            - np.e
+        )
 
 
-class Himmelblau:
+class Himmelblau(BaseOptimizationTest):
     """
     Himmelblau's function (https://en.wikipedia.org/wiki/Himmelblau%27s_function)
     """
 
-    MIN_SNR = 1e1
+    n_dof = 2
+    dofs = get_dofs(n=2)
+    bounds = np.array([[-5.0, +5.0], [-5.0, +5.0]])
 
-    def __init__(self):
-        self.n_dof = 2
-        self.dofs = get_dofs(n=2)
-        self.bounds = np.array([[0.0, 1.0] for i in range(self.n_dof)])
+    DEPENDENT_COMPONENTS = [f"x{i+1}" for i in range(2)]
 
+    @staticmethod
+    def fitness_func(X):
+        return -((X[0] ** 2 + X[1] - 11) ** 2 + (X[0] + X[1] ** 2 - 7) ** 2)
+
+
+class Rosenbrock(BaseOptimizationTest):
+    """
+    Ackley's function in $n$ dimensions (https://en.wikipedia.org/wiki/Ackley_function)
+    """
+
+    def __init__(self, n_dof=2):
+        self.n_dof = n_dof
+        self.dofs = get_dofs(n=n_dof)
+        self.bounds = np.array([[-2.0, +2.0] for i in range(self.n_dof)])
         self.DEPENDENT_COMPONENTS = [f"x{i+1}" for i in range(self.n_dof)]
 
-    def initialize(self):
-        yield from bps.null()  # do nothing
+    @staticmethod
+    def fitness_func(X):
+        return -np.log((100 * (X[..., 1:] - X[..., :-1] ** 2) ** 2 + (1 - X[..., :-1]) ** 2).sum(axis=-1))
 
-    def parse_entry(self, entry):
-        # get the ingredient from our dependent variables
-        x = 10 * (np.array([getattr(entry, f"x{i+1}") for i in range(self.n_dof)]) - 0.5)
 
-        fitness = -1e-2 * ((x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2)
+class StyblinskiTang(BaseOptimizationTest):
+    """
+    Ackley's function in $n$ dimensions (https://en.wikipedia.org/wiki/Ackley_function)
+    """
 
-        return ("fitness"), (fitness)
+    def __init__(self, n_dof=2):
+        self.n_dof = n_dof
+        self.dofs = get_dofs(n=n_dof)
+        self.bounds = np.array([[-5.0, +5.0] for i in range(self.n_dof)])
+        self.DEPENDENT_COMPONENTS = [f"x{i+1}" for i in range(self.n_dof)]
+
+    @staticmethod
+    def fitness_func(X):
+        return -(X**4 - 16 * X**2 + 5 * X).sum(axis=-1)
