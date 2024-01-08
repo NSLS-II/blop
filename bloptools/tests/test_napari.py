5f1670bce3eafb123b3ef1c90872b1b04863d11f
Thomas Morris <thomasmorris@princeton.edu>
2024-01-08 11:19:32 -0500

    added napari viewer to agent

---
+++ bloptools/tests/test_napari.py
@@ -0,0 +1,7 @@
+import pytest  # noqa F401
+
+
+@pytest.mark.parametrize("item", ["mean", "error", "qei"])
+def test_napari_viewer(agent, RE, item):
+    RE(agent.learn("qr", n=16))
+    agent.view(item)
