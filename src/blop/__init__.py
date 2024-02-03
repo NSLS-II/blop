bd83ca9b2e3acf0f551679438974642ff22b4332
Thomas Morris <thomasmorris@princeton.edu>
2024-02-03 11:38:25 -0500

    REL: v0.6.0

--- src/blop/__init__.py
+++ src/blop/__init__.py
@@ -1,8 +1,5 @@
 from . import utils  # noqa F401
-from ._version import get_versions
+from ._version import __version__, __version_tuple__  # noqa: F401
 from .agent import Agent  # noqa F401
 from .dofs import DOF  # noqa F401
 from .objectives import Objective  # noqa F401
-
-__version__ = get_versions()["version"]
-del get_versions
