019e031599bc3b181922ab8548d06b267f990a04
Thomas Morris <thomasmorris@princeton.edu>
2024-04-18 11:38:32 -0700

    saving

--- src/blop/tests/test_plots.py
+++ src/blop/tests/test_plots.py
@@ -1,22 +1,30 @@
 import pytest  # noqa F401
 
 
-def test_plots(RE, agent):
+def test_plots_one_dimensional(RE, agent_1dof_1fit):
+    agent = agent_1dof_1fit
     RE(agent.learn("qr", n=16))
+    agent.plot_objectives()
+    agent.plot_acquisition()
+    agent.plot_validity()
+    agent.plot_history()
 
+def test_plots_two_dimensional(RE, agent_1dof_1fit):
+    agent = agent_1dof_1fit
+    RE(agent.learn("qr", n=16))
     agent.plot_objectives()
     agent.plot_acquisition()
     agent.plot_validity()
     agent.plot_history()
 
 
-def test_plots_multiple_objs(RE, mo_agent):
-    RE(mo_agent.learn("qr", n=16))
-
-    mo_agent.plot_objectives()
-    mo_agent.plot_acquisition()
-    mo_agent.plot_validity()
-    mo_agent.plot_history()
+def test_plots_multiple_objs(RE, agent_2dof_2fit):
+    agent = agent_2dof_2fit
+    RE(agent.learn("qr", n=16))
+    agent.plot_objectives()
+    agent.plot_acquisition()
+    agent.plot_validity()
+    agent.plot_history()
 
 
 def test_plots_read_only_dofs(RE, agent_with_read_only_dofs):
