97c724424af56dd13d569e13afb7c142b0fea63b
Thomas Morris <thomasmorris@princeton.edu>
2024-04-18 13:45:12 -0700

    pre-commit

--- src/blop/tests/test_pareto.py
+++ src/blop/tests/test_pareto.py
@@ -6,15 +6,18 @@ def test_pareto(mo_agent, RE):
     RE(mo_agent.learn("qr", n=16))
     mo_agent.plot_pareto_front()
 
+
 @pytest.mark.parametrize("acq_func", ["qnehvi"])
 def test_monte_carlo_pareto_acq_funcs(mo_agent, RE, acq_func):
     RE(mo_agent.learn("qr", n=16))
     RE(mo_agent.learn(acq_func, n=2))
 
+
 @pytest.mark.test_func
 def test_constrained_pareto(constrained_agent, RE):
     RE(constrained_agent.learn("qr", n=16))
     constrained_agent.plot_pareto_front()
+
 
 @pytest.mark.parametrize("acq_func", ["qnehvi"])
 def test_constrained_monte_carlo_pareto_acq_funcs(constrained_agent, RE, acq_func):
