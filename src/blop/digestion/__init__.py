d4c879d948cf094adc61350e21d73589ee511b96
thomashopkins32 <thopkins1@bnl.gov>
2025-02-07 10:43:20 -0500

    Move prepare_re_env.py to examples

--- src/blop/digestion/__init__.py
+++ src/blop/digestion/__init__.py
@@ -8,5 +8,6 @@ def default_digestion_function(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def beam_stats_digestion(df: pd.DataFrame, image_key: str, **kwargs) -> pd.DataFrame:
+    # Get the beam stats for each image in the dataframe and add them as new columns
     df = pd.concat([df, df[image_key].apply(lambda img: pd.Series(get_beam_stats(img, **kwargs)))], axis=1)
     return df
