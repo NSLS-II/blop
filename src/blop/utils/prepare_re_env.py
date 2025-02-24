8c8f3c2e7eb666d5a415c9917994d79f45dd4209
jessica-moylan <jmoylan1@bnl.gov>
2025-02-24 16:57:37 -0500

    working on adding tiled

--- src/blop/utils/prepare_re_env.py
+++ src/blop/utils/prepare_re_env.py
@@ -1,16 +1,24 @@
 import argparse
 import datetime
 import json  # noqa F401
-
+from bluesky.callbacks.zmq import Publisher
 import bluesky.plan_stubs as bps  # noqa F401
 import bluesky.plans as bp  # noqa F401
+
+from tiled.client import from_uri, from_profile ##
+import time as ttime
+from bluesky.callbacks.tiled_writer import TiledWriter
+
 import matplotlib.pyplot as plt
 import numpy as np  # noqa F401
 from bluesky.callbacks import best_effort
 from bluesky.callbacks.tiled_writer import TiledWriter
 from bluesky.run_engine import RunEngine
 from ophyd.utils import make_dir_tree
-from tiled.client import from_uri  #
+from tiled.client.utils import handle_error
+from tiled.utils import safe_json_dump
+
+from blop.sim import HDF5Handler
 
 DEFAULT_DB_TYPE = "local"
 DEFAULT_ROOT_DIR = "/tmp/sirepo-bluesky-data"
@@ -23,17 +31,21 @@ tiled_client = from_uri(SERVER_HOST_LOCATION, api_key="secret")
 
 tiled_writer = TiledWriter(tiled_client)
 
+SERVER_HOST_LOCATION = "http://localhost:8000"
 
 def re_env(db_type="default", root_dir="/default/path"):
     RE = RunEngine({})
     bec = best_effort.BestEffortCallback()
-    RE.subscribe(bec)
+    RE.subscribe(bec)    # Setup Tiled Client
 
     RE.subscribe(tiled_writer)
 
     _ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)
     return {"RE": RE, "db": tiled_client, "bec": bec}
 
+    _ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)    
+    return dict(RE=RE, db=tiled_client, bec=bec)
+    
 
 def register_handlers(db, handlers):
     for handler_spec, handler_class in handlers.items():
