3ad3aa568803260942b86ffb93ea517e0e38fae1
thomashopkins32 <thopkins1@bnl.gov>
2025-04-18 14:49:10 -0400

    Add Ax optional dependency; Make the unit test more like the demo notebook

--- src/blop/tests/integrations/test_ax.py
+++ src/blop/tests/integrations/test_ax.py
@@ -38,17 +38,16 @@ def test_ax_client_experiment(RE, db):
         ],
         objectives={
             "beam_intensity": ObjectiveProperties(minimize=False, threshold=200.0),
-            "beam_width": ObjectiveProperties(minimize=True, threshold=50.0),
-            "beam_height": ObjectiveProperties(minimize=True, threshold=50.0),
+            "beam_area": ObjectiveProperties(minimize=True, threshold=2000.0),
         },
     )
 
     def evaluate(results_df: pd.DataFrame) -> dict[str, tuple[float, float]]:
         stats = get_beam_stats(results_df["bl_det_image"].iloc[0])
+        area = stats["wid_x"] * stats["wid_y"]
         return {
             "beam_intensity": (stats["sum"], None),
-            "beam_width": (stats["wid_x"], None),
-            "beam_height": (stats["wid_y"], None),
+            "beam_area": (area, None),
         }
 
     evaluator = create_bluesky_evaluator(
