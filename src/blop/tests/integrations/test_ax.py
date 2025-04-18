c99d723abb2c628e83c61afdf5d4bb31b7df58d2
thomashopkins32 <thopkins1@bnl.gov>
2025-04-18 14:40:08 -0400

    Added notebook tutorial for Ax + Blop

--- src/blop/tests/integrations/test_ax.py
+++ src/blop/tests/integrations/test_ax.py
@@ -36,18 +36,24 @@ def test_ax_client_experiment(RE, db):
                 "bounds": [-5.0, 5.0],
             },
         ],
-        objectives={"beam_intensity": ObjectiveProperties(minimize=False), "beam_area": ObjectiveProperties(minimize=True)},
+        objectives={
+            "beam_intensity": ObjectiveProperties(minimize=False, threshold=200.0),
+            "beam_width": ObjectiveProperties(minimize=True, threshold=50.0),
+            "beam_height": ObjectiveProperties(minimize=True, threshold=50.0),
+        },
     )
 
     def evaluate(results_df: pd.DataFrame) -> dict[str, tuple[float, float]]:
         stats = get_beam_stats(results_df["bl_det_image"].iloc[0])
-        return {"beam_intensity": (stats["sum"], None), "beam_area": (stats["area"], None)}
+        return {
+            "beam_intensity": (stats["sum"], None),
+            "beam_width": (stats["wid_x"], None),
+            "beam_height": (stats["wid_y"], None),
+        }
 
     evaluator = create_bluesky_evaluator(
         RE, db, [beamline.det], [beamline.kbv_dsv, beamline.kbv_usv, beamline.kbh_dsh, beamline.kbh_ush], evaluate
     )
-    for _ in range(25):
+    for _ in range(10):
         parameterization, trial_index = ax_client.get_next_trial()
         ax_client.complete_trial(trial_index=trial_index, raw_data=evaluator(parameterization))
-
-    print(ax_client.generation_strategy.trials_as_df)
