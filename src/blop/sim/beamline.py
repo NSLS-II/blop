de799f5e8dfecafd779afd9a3243f7306177eec6
jessica-moylan <jessicamoylan10@gmail.com>
2025-05-19 16:39:00 -0400

    fixed initialization in tutorials

--- src/blop/sim/beamline.py
+++ src/blop/sim/beamline.py
@@ -41,7 +41,7 @@ class Detector(Device):
     ) -> None:
         super().__init__(*args, **kwargs)
 
-        _ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)
+        _ = make_dir_tree(datetime.now().year, base_path=root_dir)
 
         self._root_dir = root_dir
         self._verbose = verbose
@@ -74,14 +74,12 @@ class Detector(Device):
         self._dataset.resize((current_frame + 1, *self.image_shape.get()))
         self._dataset[current_frame, :, :] = raw_image
 
-        # datum_document = self._datum_factory(datum_kwargs={"frame": current_frame})
-        stream_datum_document = self._stream_datum_factory(
-            StreamRange(start=current_frame, stop=current_frame + 1),
-        )
-        self._asset_docs_cache.append(("stream_datum", stream_datum_document))
+        datum_document = self._datum_factory(datum_kwargs={"frame": current_frame})
+
+        self._asset_docs_cache.append(("stream_datum", datum_document))
 
         stats = get_beam_stats(raw_image)
-        # self.image.put(stream_datum_document["datum_id"])
+        self.image.put(datum_document["datum_id"])
 
         for attr in ["max", "sum", "cen_x", "cen_y", "wid_x", "wid_y"]:
             getattr(self, attr).put(stats[attr])
@@ -90,44 +88,19 @@ class Detector(Device):
 
         return NullStatus()
     
-    def _generate_file_path(self, date_template="%Y/%m/%d"):
-        date = datetime.datetime.now()
-        assets_dir = date.strftime(date_template)
-        data_file = f"{new_uid()}.h5"
-
-        return Path(self._root_dir) / Path(assets_dir) / Path(data_file)
-
 
     def stage(self) -> list[Any]:
         devices = super().stage()
         date = datetime.now()
         self._assets_dir = date.strftime("%Y/%m/%d")
         data_file = f"{new_uid()}.h5"
-
-        # self._resource_document, self._datum_factory, _ = compose_resource(
-        #     start={"uid": "needed for compose_resource() but will be discarded"},
-        #     spec="hdf5", #made this lowercase
-        #     root=self._root_dir,
-        #     resource_path=str(Path(self._assets_dir) / Path(data_file)),
-        #     resource_kwargs={},
-        # )
-        self._asset_docs_cache.clear()
-        full_path = self._generate_file_path()
-        image_shape = self.image_shape.get()
-
-        uri = f"file://localhost/{str(full_path).strip('/')}"
-
-        (
-            self._stream_resource_document,
-            self._stream_datum_factory,
-        ) = compose_stream_resource(
-            mimetype="application/x-hdf5",
-            uri=uri,
-            data_key=self.image.name,
-            parameters={
-                "chunk_shape": (1, *image_shape),
-                "dataset": "/entry/image",
-            },
+        
+        self._resource_document, self._datum_factory, _ = compose_resource(
+            start={"uid": "needed for compose_resource() but will be discarded"},
+            spec="HDF5",
+            root=self._root_dir,
+            resource_path=str(Path(self._assets_dir) / Path(data_file)),
+            resource_kwargs={},
         )
 
         if not self._resource_document:
@@ -135,17 +108,17 @@ class Detector(Device):
 
         self._data_file = str(Path(self._resource_document["root"]) / Path(self._resource_document["resource_path"]))
 
-        self._asset_docs_cache.append(
-            ("stream_resource", self._stream_resource_document)
-        )
+        self._resource_document.pop("run_start")
+        self._asset_docs_cache.append(("resource", self._resource_document))
+
 
         self._h5file_desc = h5py.File(self._data_file, "x")
         group = self._h5file_desc.create_group("/entry")
         self._dataset = group.create_dataset(
             "image",
-            data=np.full(fill_value=np.nan, shape=(1, *image_shape)),
-            maxshape=(None, *image_shape),
-            chunks=(1, *image_shape),
+            data=np.full(fill_value=np.nan, shape=(1, *self.image_shape.get())),
+            maxshape=(None, *self.image_shape.get()),
+            chunks=(1, *self.image_shape.get()),
             dtype="float64",
             compression="lzf",
         )
