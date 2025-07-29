47677b68b767196f4a8e38aa71e1d6d8f3283e6b
jessica-moylan <jessicamoylan10@gmail.com>
2025-07-29 18:03:47 -0400

    converted databroker to use dictonaries

--- src/blop/digestion/tests.py
+++ src/blop/digestion/tests.py
@@ -4,52 +4,56 @@ import pandas as pd
 from ..utils import functions
 
 
-def himmelblau_digestion(df: pd.DataFrame) -> pd.DataFrame:
+def himmelblau_digestion(df: dict) -> dict:
     """
     Digests Himmelblau's function into the feedback.
     """
-    df["x1"] = df["x1"].fillna(0) if "x1" in df.columns else 0
-    df["x2"] = df["x2"].fillna(0) if "x2" in df.columns else 0
-    df["himmelblau"] = functions.himmelblau(x1=df.x1, x2=df.x2)
-    df["himmelblau_transpose"] = functions.himmelblau(x1=df.x2, x2=df.x1)
+    num_rows = len(next(iter(df.values())))
+
+    df["x1"] = [0 if val is None else val for val in df.get("x1", [0] * num_rows)]
+    df["x2"] = [0 if val is None else val for val in df.get("x2", [0] * num_rows)]
+    df["himmelblau"] = [functions.himmelblau(x1=df["x1"][i], x2=df["x2"][i]) for i in range(num_rows)]
+    df["himmelblau_transpose"] = [functions.himmelblau(x1=df["x2"][i], x2=df["x1"][i]) for i in range(num_rows)]
+
     return df
 
 
-def constrained_himmelblau_digestion(df: pd.DataFrame) -> pd.DataFrame:
+def constrained_himmelblau_digestion(df):
     """
     Digests Himmelblau's function into the feedback, constrained with NaN for a distance of more than 6 from the origin.
     """
-
     df = himmelblau_digestion(df)
-    df.loc[:, "himmelblau"] = np.where(
-        np.array(df.x1.values) ** 2 + np.array(df.x2) ** 2 < 36, np.array(df.himmelblau), np.nan
-    )
-
+    df["himmelblau"] = [
+        np.nan if (x1_val**2 + x2_val**2 >= 36) else himmelblau_val
+        for x1_val, x2_val, himmelblau_val in zip(df["x1"], df["x2"], df["himmelblau"])
+    ]
     return df
 
 
-def sketchy_himmelblau_digestion(df: pd.DataFrame, p: float = 0.1) -> pd.DataFrame:
+def sketchy_himmelblau_digestion(df, p: float = 0.1): #-> pd.DataFrame:
     """
     Evaluates the constrained Himmelblau, where every point is bad with probability p.
     """
 
     df = constrained_himmelblau_digestion(df)
-    bad = np.random.choice(a=[True, False], size=len(df), p=[p, 1 - p])
-    df.loc[:, "himmelblau"] = np.where(bad, np.nan, np.array(df.himmelblau))
-
+    bad = np.random.choice(a=[True, False], size=len(next(iter(df.values()))), p=[p, 1 - p])
+    df['himmelblau'] = [
+        np.nan if is_bad else val
+        for val, is_bad in zip(df['himmelblau'], bad)
+    ]
     return df
 
-
-"""
-Chankong and Haimes function from https://en.wikipedia.org/wiki/Test_functions_for_optimization
-"""
-
-
 def chankong_and_haimes_digestion(df: pd.DataFrame) -> pd.DataFrame:
-    df["f1"] = (df.x1 - 2) ** 2 + (df.x2 - 1) + 2
-    df["f2"] = 9 * df.x1 - (df.x2 - 1) + 2
-    df["c1"] = df.x1**2 + df.x2**2
-    df["c2"] = df.x1 - 3 * df.x2 + 10
+    """
+    Chankong and Haimes function from https://en.wikipedia.org/wiki/Test_functions_for_optimization
+    """
+    
+    df["f1"], df["f2"], df["c1"], df["c2"] = [], [], [], []
+    for (val_x1, val_x2) in zip(df.get("x1"), df.get("x2")):
+        df["f1"].append((val_x1 - 2) ** 2 + (val_x2 - 1) + 2)
+        df["f2"].append(9 * val_x1 - (val_x2 - 1) + 2)
+        df["c1"].append(val_x1**2 + val_x2**2)
+        df["c2"].append(val_x1 - 3 * val_x2 + 10)
     return df
 
 
