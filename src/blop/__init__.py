3b977f592e49e4f662689b17227d77832e030cc2
thomashopkins32 <thopkins1@bnl.gov>
2025-10-03 11:56:15 -0400

    Remove version import

--- src/blop/__init__.py
+++ src/blop/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
 from . import utils  # noqa F401
-from ._version import __version__, __version_tuple__  # noqa: F401
 from .agent import Agent  # noqa F401
 from .dofs import DOF  # noqa F401
 from .objectives import Objective  # noqa F401
