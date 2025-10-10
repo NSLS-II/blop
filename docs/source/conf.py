daf50f94bd17f0056bc79caa5d48bf1fb6b1fe91
Thomas Hopkins <thopkins1@bnl.gov>
2025-10-10 14:34:09 -0400

    Remove scipy from intersphinx mapping
    Removed scipy from intersphinx mapping.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -232,7 +232,6 @@ texinfo_documents = [
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
 }
