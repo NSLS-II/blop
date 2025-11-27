c92694712faaf50d37949907b06ee309280289aa
Thomas Hopkins <thopkins1@bnl.gov>
2025-11-27 11:20:25 -0500

    Simplified DOF classes for Ax backend (#195)
    * Simplified DOF classes for Ax backend
    
    * Make DOF abstract

---
+++ src/blop/tests/unit/ax/test_dof.py
@@ -0,0 +1,71 @@
+import pytest
+from ax import ChoiceParameterConfig, RangeParameterConfig
+
+from blop.ax.dof import DOF, ChoiceDOF, RangeDOF
+
+from ..conftest import MovableSignal
+
+
+def test_dof_movable():
+    movable = MovableSignal(name="test_movable")
+    range_dof = RangeDOF(movable=movable, bounds=(0, 1), parameter_type="float", step_size=0.1, scaling="linear")
+    assert range_dof.parameter_name == "test_movable"
+    choice_dof = ChoiceDOF(
+        movable=movable, values=[0, 1, 2, 3, 4, 5], parameter_type="int", is_ordered=True, dependent_parameters=None
+    )
+    assert choice_dof.parameter_name == "test_movable"
+
+
+def test_dof_name():
+    range_dof = RangeDOF(name="test_name", bounds=(0, 1), parameter_type="float", step_size=0.1, scaling="linear")
+    assert range_dof.parameter_name == "test_name"
+    choice_dof = ChoiceDOF(
+        name="test_name", values=[0, 1, 2, 3, 4, 5], parameter_type="int", is_ordered=True, dependent_parameters=None
+    )
+    assert choice_dof.parameter_name == "test_name"
+
+
+def test_dof_invalid():
+    movable = MovableSignal(name="test_movable")
+    with pytest.raises(ValueError):
+        RangeDOF(movable=movable, name="test_movable", bounds=(0, 1), parameter_type="float")
+
+    with pytest.raises(ValueError):
+        ChoiceDOF(movable=movable, name="test_name", values=[0, 1, 2, 3, 4, 5], parameter_type="int")
+
+    with pytest.raises(TypeError):
+        DOF(movable=movable, name="test_movable")  # type: ignore[arg-type]
+
+
+def test_range_dof():
+    dof1 = RangeDOF(name="test_name", bounds=(0, 1), parameter_type="float", step_size=0.1, scaling="linear")
+
+    assert dof1.to_ax_parameter_config() == RangeParameterConfig(
+        name="test_name", bounds=(0, 1), parameter_type="float", step_size=0.1, scaling="linear"
+    )
+
+    movable = MovableSignal(name="test_movable")
+    dof2 = RangeDOF(movable=movable, bounds=(0, 1), parameter_type="float", step_size=0.1, scaling="linear")
+
+    assert dof2.to_ax_parameter_config() == RangeParameterConfig(
+        name="test_movable", bounds=(0, 1), parameter_type="float", step_size=0.1, scaling="linear"
+    )
+
+
+def test_choice_dof():
+    dof1 = ChoiceDOF(
+        name="test_name", values=[0, 1, 2, 3, 4, 5], parameter_type="int", is_ordered=True, dependent_parameters=None
+    )
+
+    assert dof1.to_ax_parameter_config() == ChoiceParameterConfig(
+        name="test_name", values=[0, 1, 2, 3, 4, 5], parameter_type="int", is_ordered=True, dependent_parameters=None
+    )
+
+    movable = MovableSignal(name="test_movable")
+    dof2 = ChoiceDOF(
+        movable=movable, values=[0, 1, 2, 3, 4, 5], parameter_type="int", is_ordered=True, dependent_parameters=None
+    )
+
+    assert dof2.to_ax_parameter_config() == ChoiceParameterConfig(
+        name="test_movable", values=[0, 1, 2, 3, 4, 5], parameter_type="int", is_ordered=True, dependent_parameters=None
+    )
