190b195a1432ff63fe4000c7c80ea98c988ea6eb
Thomas Hopkins <thomashopkins000@gmail.com>
2025-12-21 10:54:53 -0500

    Updated dependencies

--- src/blop/__init__.py
+++ src/blop/__init__.py
@@ -1,6 +1,12 @@
 from .ax import Agent, ChoiceDOF, DOFConstraint, Objective, OutcomeConstraint, RangeDOF, ScalarizedObjective
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
 __all__ = [
+    "__version__",
     "Agent",
     "RangeDOF",
     "ChoiceDOF",
